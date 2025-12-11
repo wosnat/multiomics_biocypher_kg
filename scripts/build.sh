2317c6b991cbaf5f994c81612def72a761357d23
Osnat Weissberg <osnat.weissberg@gmail.com>
2025-12-11 11:19:54 +0200

    clean build in docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,11 @@ cp -r /src/* .
 cp config/biocypher_docker_config.yaml config/biocypher_config.yaml
 pip install uv
 uv sync
+# add clean build (set via env variable in docker-compose.yml)
+if [ "${BUILD2NEO_CLEANUP}" == 'yes' ]; then
+        echo "BUILD2NEO_CLEANUP is yes"
+        echo "rm -rf /usr/app/data/build2neo"
+        rm -rf /usr/app/data/build2neo
+fi
 uv run python3 create_knowledge_graph.py
 chmod -R 777 biocypher-log
